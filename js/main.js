e17b05e7f58ec5596c56dbf60f04ba2fe3a577cb
Lesha <lexaxiip@gmail.com>
2024-02-15 21:27:27 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-var svgNav = document.querySelector('.header__nav-svg-one')
-var inputNav = document.querySelector('.header__nav-input-one')
-
-svgNav.addEventListener('click', () => {
-	inputNav.classList.toggle('header__nav-input-one--active')
-=======
 var svgNav = document.querySelector('.header__nav-svg')
 var inputNav = document.querySelector('.header__nav-input')
 
 svgNav.addEventListener('click', () => {
 	inputNav.classList.toggle('header__nav-input--active')
->>>>>>> 06e6663 (fix nav)
 })
