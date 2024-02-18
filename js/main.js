388c8f308d97a0fea23949e6588de58357778e7e
Lesha <lexaxiip@gmail.com>
2024-02-18 14:41:39 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -5,31 +5,6 @@ svgNav.addEventListener('click', () => {
 	inputNav.classList.toggle('header__nav-input--active')
 })
 
-// const listPriceBtn = document.querySelector(
-// 		'.top-catalog__contants-filter-price'
-// 	),
-// 	itemsPrice = document.querySelectorAll('.top-catalog__contants-list-item')
-
-// function filter() {
-// 	listPriceBtn.addEventListener('change', event => {
-// 		const targetId = event.target.dataset.id
-
-// 		switch (targetId) {
-// 			case 'price2':
-// 				itemsPrice.forEach(item => {
-// 					if (item.classList.contains('price2')) {
-// 						item.style.display = 'block'
-// 					} else {
-// 						item.style.display = 'none'
-// 					}
-// 				})
-// 				break
-// 		}
-// 	})
-// }
-
-// filter()
-
 const filter = document.querySelector('.top-catalog__contants-filter-price')
 
 filter.addEventListener('change', function () {
